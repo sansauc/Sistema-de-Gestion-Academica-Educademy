0f1f41963253e855fda7a42636454de01bec05ce
sansauc <santiagosaucedocrg@gmail.com>
2025-10-27 11:49:52 -0300

    Se modifico el metodo de delete de una materia, ahora primero borra la bidireccionalidad que hay con los cursos y profesores, antes de borrar la materia en cuestion

--- src/main/java/com/sansa/practica/springboot/app/springboot_project_educademy/services/MateriaServiceImpl.java
+++ src/main/java/com/sansa/practica/springboot/app/springboot_project_educademy/services/MateriaServiceImpl.java
@@ -43,37 +43,59 @@ public class MateriaServiceImpl implements MateriaService {
     @Transactional
     public Optional<Materia> delete(Long id) {
         Optional<Materia> materiaOptional = repository.findById(id);
+        
         materiaOptional.ifPresent(materiaDb -> {
+  
+            //Quitamos los Cursos y Profesores de las listas, para poder borrar la materia, evitamos el nullpointerexception si las listas son null
+            //Primero se elimina la referencia de la materia tanto en cursos como en profesores antes de borrarla
+            
+            if (materiaDb.getCursos() != null){
+                materiaDb.getCursos().forEach(curso -> {
+                    if(curso.getMaterias() != null){
+                        curso.getMaterias().remove(materiaDb);
+                    }
+                });
+                materiaDb.getCursos().clear();
+            }
+
+            if (materiaDb.getProfesores() != null){
+                materiaDb.getProfesores().forEach(profesor -> {
+                    if (profesor.getMateriasDictadas() != null){
+                        profesor.getMateriasDictadas().remove(materiaDb);
+                    }
+                });
+                materiaDb.getProfesores().clear();
+            }
             repository.delete(materiaDb);
         });
+
         return materiaOptional;
     }
-    
+
     @Override
     public Optional<Materia> saveIfNotExists(Materia materia) {
-        
-        //Valida por el nombre
-        Optional<Materia> existingByDni = repository.findByNombre(materia.getNombre());
-        if (existingByDni.isPresent()){
-            return Optional.empty(); //Ya existe una materia con ese nombre
+
+        // Valida por el nombre
+        Optional<Materia> existingByNombre = repository.findByNombre(materia.getNombre());
+        if (existingByNombre.isPresent()) {
+            return Optional.empty(); // Ya existe una materia con ese nombre
         }
 
         Materia saved = repository.save(materia);
         return Optional.of(saved);
     }
 
-
-    //Para usar estos ultimos 2 metodos el profesor debe existir
+    // Para usar estos ultimos 2 metodos el profesor debe existir
     @Override
     @Transactional
     public Optional<Materia> agregarProfesor(Long idMateria, Profesor profesor) {
         Optional<Materia> materOptional = repository.findById(idMateria);
-        if(materOptional.isPresent()){
+        if (materOptional.isPresent()) {
             Materia materiaBd = materOptional.get();
 
-            //Recuperar al profesor
+            // Recuperar al profesor
             Optional<Profesor> profOptional = repository2.findByProfesorId(profesor.getProfesorId());
-            if(profOptional.isEmpty()){
+            if (profOptional.isEmpty()) {
                 return Optional.empty();
             }
 
@@ -84,14 +106,15 @@ public class MateriaServiceImpl implements MateriaService {
     }
 
     @Override
+    @Transactional
     public Optional<Materia> quitarProfesor(Long idMateria, Profesor profesor) {
         Optional<Materia> materOptional = repository.findById(idMateria);
-        if(materOptional.isPresent()){
+        if (materOptional.isPresent()) {
             Materia materiaBd = materOptional.get();
 
-            //Recuperar al profesor
+            // Recuperar al profesor
             Optional<Profesor> profOptional = repository2.findByProfesorId(profesor.getProfesorId());
-            if(profOptional.isEmpty()){
+            if (profOptional.isEmpty()) {
                 return Optional.empty();
             }
 
@@ -100,6 +123,5 @@ public class MateriaServiceImpl implements MateriaService {
         }
         return materOptional;
     }
-
 
 }
