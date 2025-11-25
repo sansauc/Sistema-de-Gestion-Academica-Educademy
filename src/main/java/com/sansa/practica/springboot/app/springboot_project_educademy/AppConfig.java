aa059b7b9d2a091b77a0f4f3d18286b7a8db2d22
sansauc <santiagosaucedocrg@gmail.com>
2025-11-25 11:00:37 -0300

    Se creo una clase AppConfig para el bean del propertysource, por ello se quito este bean de la clase principal, donde esta el main, y se agrego a appconfig, el funcionamiento sigue igual

---
+++ src/main/java/com/sansa/practica/springboot/app/springboot_project_educademy/AppConfig.java
@@ -0,0 +1,10 @@
+package com.sansa.practica.springboot.app.springboot_project_educademy;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:messages.properties")
+public class AppConfig {
+
+}
