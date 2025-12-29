854bd829845e61b928a38c20d82dc3b5d5690d23
sansauc <santiagosaucedocrg@gmail.com>
2025-12-29 11:59:24 -0300

    Se implemento el metodo que genera el token y lo devuelve como json

--- src/main/java/com/sansa/practica/springboot/app/springboot_project_educademy/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/sansa/practica/springboot/app/springboot_project_educademy/security/filter/JwtAuthenticationFilter.java
@@ -1,24 +1,33 @@
 package com.sansa.practica.springboot.app.springboot_project_educademy.security.filter;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.SecretKey;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sansa.practica.springboot.app.springboot_project_educademy.entities.User;
 
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
 
     private AuthenticationManager authenticationManager;
+
+    private static final SecretKey  SECRET_KEY = Jwts.SIG.HS256.key().build();
 
     public JwtAuthenticationFilter(AuthenticationManager authenticationManager){
         this.authenticationManager = authenticationManager;
@@ -52,6 +61,34 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 return authenticationManager.authenticate(authenticationToken);
     }
 
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+     
+                User user = (User)authResult.getPrincipal();
+                String username = user.getUsername();
+                
+                String token = Jwts.builder()
+                    .subject(username)
+                    .signWith(SECRET_KEY)
+                    .compact(); //Esto genera el token
+
+                response.addHeader("Authorization","Bearer " + token);
+
+                Map<String, String> body = new HashMap<>();
+                body.put("token", token);
+                body.put("username", username);
+                body.put("message", "Hola " + username + " has iniciado sesión con exito");
+
+                response.getWriter().write(new ObjectMapper().writeValueAsString(body)); //generamos el json 
+                response.setContentType("application/json");
+                response.setStatus(200);
+
+        
+    }
+//Quedamos en 2:01:00
+
+    
     
     
 }
